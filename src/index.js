6d16b833b037b6500ec6975dbb864be210c101fb
Rocky Fatafehi <coder97006@gmail.com>
2019-06-29 10:11:04 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+ 
+ 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div>
+    
+    <h1>React Newbie!</h1>
+    <p>This is my first React App</p>
+    <ul>
+        <li>React for UI FrontEnd</li>
+        <li>Mongo for Backend</li>
+        <li>Node.js for BE logic</li>
+        <li>Python for AI</li>
+    </ul>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+
+
+</div>, document.getElementById('root'));
+
+ReactDOM.render(<div>
+    
+    <h1>React Newbie!</h1>
+    <p>This is my first React App</p>
+    <ul>
+        <li>React for UI FrontEnd</li>
+        <li>Mongo for Backend</li>
+        <li>Node.js for BE logic</li>
+        <li>Python for AI</li>
+    </ul>
+
+
+
+
+
+</div>, document.getElementById('root2'));
+ 
